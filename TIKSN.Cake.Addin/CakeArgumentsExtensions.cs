eb017649623309f5005234e718c9fcc682ed3d19
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-09-30 21:51:21 +0300

    extension added

---
+++ TIKSN.Cake.Addin/CakeArgumentsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Core;
+
+namespace TIKSN.Cake.Addin
+{
+    public static class CakeArgumentsExtensions
+    {
+        public static string GetArgument(this ICakeArguments cakeArguments, string name)
+        {
+            if (!cakeArguments.HasArgument(name))
+                return null;
+
+            return cakeArguments.GetArgument(name);
+        }
+    }
+}
