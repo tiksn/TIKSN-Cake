8881f88e4eb3873d7bff0e2c6ee3b40a0fdb4d67
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-10-02 00:23:43 +0300

    NextRevisionVersioningStrategy added

--- TIKSN.Cake.Core/Services/VersioningStrategies/NextRevisionVersioningStrategy.cs
+++ TIKSN.Cake.Core/Services/VersioningStrategies/NextRevisionVersioningStrategy.cs
@@ -1,12 +1,26 @@
-﻿using TIKSN.Versioning;
+﻿using System;
+using TIKSN.Time;
 
 namespace TIKSN.Cake.Core.Services.VersioningStrategies
 {
     public class NextRevisionVersioningStrategy : IVersioningStrategy
     {
-        public Version GetNextVersion(Version latestVersion)
+        private readonly ITimeProvider _timeProvider;
+
+        public NextRevisionVersioningStrategy(ITimeProvider timeProvider)
         {
-            throw new System.NotImplementedException();
+            _timeProvider = timeProvider ?? throw new ArgumentNullException(nameof(timeProvider));
+        }
+
+        public Versioning.Version GetNextVersion(Versioning.Version latestVersion)
+        {
+            var nextRelease = new Version(
+                latestVersion.Release.Major,
+                latestVersion.Release.Minor,
+                latestVersion.Release.Build == -1 ? 0 : latestVersion.Release.Build,
+                latestVersion.Release.Revision == -1 || latestVersion.Release.Revision == 0 ? 1 : latestVersion.Release.Revision + 1);
+
+            return new Versioning.Version(nextRelease, _timeProvider.GetCurrentTime());
         }
     }
 }
