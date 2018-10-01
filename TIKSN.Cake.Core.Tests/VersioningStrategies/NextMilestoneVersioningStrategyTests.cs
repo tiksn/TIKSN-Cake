f9b84b2a53326db525c49eb45f56c41cd4a4e079
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-10-01 23:55:37 +0300

    unit tests added

--- TIKSN.Cake.Core.Tests/VersioningStrategies/NextMilestoneVersioningStrategyTests.cs
+++ TIKSN.Cake.Core.Tests/VersioningStrategies/NextMilestoneVersioningStrategyTests.cs
@@ -1,6 +1,47 @@
-﻿namespace TIKSN.Cake.Core.Tests.VersioningStrategies
+﻿using FluentAssertions;
+using NSubstitute;
+using NuGet.Versioning;
+using TIKSN.Cake.Core.Services.VersioningStrategies;
+using TIKSN.Time;
+using TIKSN.Versioning;
+using Xunit;
+
+namespace TIKSN.Cake.Core.Tests.VersioningStrategies
 {
     public class NextMilestoneVersioningStrategyTests
     {
+        [Theory]
+        [InlineData("1.2.3-alpha.1", "1.2.3-beta.1")]
+        [InlineData("1.2.3-alpha.12", "1.2.3-beta.1")]
+        [InlineData("1.2.3-beta.14", "1.2.3-rc.1")]
+        [InlineData("1.2.3-rc.14", "1.2.3")]
+        [InlineData("1.2.3.4-alpha.1", "1.2.3.4-beta.1")]
+        [InlineData("1.2.3.4-alpha.12", "1.2.3.4-beta.1")]
+        [InlineData("1.2.3.4-beta.14", "1.2.3.4-rc.1")]
+        [InlineData("1.2.3.4-rc.14", "1.2.3.4")]
+        public void GetNextVersionTest(string latestVersionString, string expectedNextVersionString)
+        {
+            var latestVersion = (Version)NuGetVersion.Parse(latestVersionString);
+            var expectedNextVersion = (Version)NuGetVersion.Parse(expectedNextVersionString);
+            var timeProvider = Substitute.For<ITimeProvider>();
+            var strategy = new NextMilestoneVersioningStrategy(timeProvider);
+
+            var nextVersion = strategy.GetNextVersion(latestVersion);
+
+            nextVersion.Should().Be(expectedNextVersion);
+        }
+
+        [Theory]
+        [InlineData("1.2.0")]
+        [InlineData("1.2.3")]
+        [InlineData("1.2.3.4")]
+        public void GetNextVersionThrowsException(string latestVersionString)
+        {
+            var latestVersion = (Version)NuGetVersion.Parse(latestVersionString);
+            var timeProvider = Substitute.For<ITimeProvider>();
+            var strategy = new NextMilestoneVersioningStrategy(timeProvider);
+
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => strategy.GetNextVersion(latestVersion));
+        }
     }
 }
--- TIKSN.Cake.Core/Services/VersioningStrategies/NextMilestoneVersioningStrategy.cs
+++ TIKSN.Cake.Core/Services/VersioningStrategies/NextMilestoneVersioningStrategy.cs
@@ -1,10 +1,18 @@
-﻿using TIKSN.Versioning;
+﻿using System;
+using TIKSN.Time;
 
 namespace TIKSN.Cake.Core.Services.VersioningStrategies
 {
     public class NextMilestoneVersioningStrategy : IVersioningStrategy
     {
-        public Version GetNextVersion(Version latestVersion)
+        private readonly ITimeProvider _timeProvider;
+
+        public NextMilestoneVersioningStrategy(ITimeProvider timeProvider)
+        {
+            _timeProvider = timeProvider ?? throw new ArgumentNullException(nameof(timeProvider));
+        }
+
+        public Versioning.Version GetNextVersion(Versioning.Version latestVersion)
         {
             throw new System.NotImplementedException();
         }
