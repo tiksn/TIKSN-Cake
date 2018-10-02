8d3da597dfefb80e6be26187327dbba59a99dae8
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-10-02 23:57:31 +0300

    minor version strategy implementation and tests

--- TIKSN.Cake.Core.Tests/VersioningStrategies/NextMinorVersioningStrategyTests.cs
+++ TIKSN.Cake.Core.Tests/VersioningStrategies/NextMinorVersioningStrategyTests.cs
@@ -1,6 +1,38 @@
-﻿namespace TIKSN.Cake.Core.Tests.VersioningStrategies
+﻿using FluentAssertions;
+using NSubstitute;
+using NuGet.Versioning;
+using TIKSN.Cake.Core.Services.VersioningStrategies;
+using TIKSN.Time;
+using TIKSN.Versioning;
+using Xunit;
+
+namespace TIKSN.Cake.Core.Tests.VersioningStrategies
 {
     public class NextMinorVersioningStrategyTests
     {
+        [Theory]
+        [InlineData("1.2.3-alpha.1", "1.3.0")]
+        [InlineData("1.2.3-alpha.12", "1.3.0")]
+        [InlineData("1.2.3-beta.14", "1.3.0")]
+        [InlineData("1.2.3-rc.14", "1.3.0")]
+        [InlineData("1.2.3.4-alpha.1", "1.3.0")]
+        [InlineData("1.2.3.4-alpha.12", "1.3.0")]
+        [InlineData("1.2.3.4-beta.14", "1.3.0")]
+        [InlineData("1.2.3.4-rc.14", "1.3.0")]
+        [InlineData("1.2.0", "1.3.0")]
+        [InlineData("1.2", "1.3.0")]
+        [InlineData("1.2.3", "1.3.0")]
+        [InlineData("1.2.3.4", "1.3.0")]
+        public void GetNextVersionTest(string latestVersionString, string expectedNextVersionString)
+        {
+            var latestVersion = (Version)NuGetVersion.Parse(latestVersionString);
+            var expectedNextVersion = (Version)NuGetVersion.Parse(expectedNextVersionString);
+            var timeProvider = Substitute.For<ITimeProvider>();
+            var strategy = new NextMinorVersioningStrategy(timeProvider);
+
+            var nextVersion = strategy.GetNextVersion(latestVersion);
+
+            nextVersion.Should().Be(expectedNextVersion);
+        }
     }
 }
--- TIKSN.Cake.Core/Services/VersioningStrategies/NextMinorVersioningStrategy.cs
+++ TIKSN.Cake.Core/Services/VersioningStrategies/NextMinorVersioningStrategy.cs
@@ -1,12 +1,26 @@
-﻿using TIKSN.Versioning;
+﻿using System;
+using TIKSN.Time;
 
 namespace TIKSN.Cake.Core.Services.VersioningStrategies
 {
     public class NextMinorVersioningStrategy : IVersioningStrategy
     {
-        public Version GetNextVersion(Version latestVersion)
+        private readonly ITimeProvider _timeProvider;
+
+        public NextMinorVersioningStrategy(ITimeProvider timeProvider)
         {
-            throw new System.NotImplementedException();
+            _timeProvider = timeProvider ?? throw new System.ArgumentNullException(nameof(timeProvider));
+        }
+
+        public Versioning.Version GetNextVersion(Versioning.Version latestVersion)
+        {
+            var nextRelease = new Version(
+                latestVersion.Release.Major,
+                latestVersion.Release.Minor == -1 ? 1 : latestVersion.Release.Minor + 1,
+                0,
+                0);
+
+            return new Versioning.Version(nextRelease, _timeProvider.GetCurrentTime());
         }
     }
 }
