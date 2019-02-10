a616a79138415d032491f733b06d0103d6ba64fb
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-02-11 00:08:29 +0200

    test added

---
+++ TIKSN.Cake.Core.Tests/TrashFolderServicesTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using IdGen;
+using Microsoft.Extensions.DependencyInjection;
+using TIKSN.Cake.Core.Services;
+using Xunit;
+
+namespace TIKSN.Cake.Core.Tests
+{
+    public class TrashFolderServicesTests
+    {
+        [Fact]
+        public void ResolveDependency()
+        {
+            var serviceProvider = DependencyInjectionHelper.CreateServiceProvider();
+
+            var trashFolderServices = serviceProvider.GetRequiredService<ITrashFolderServices>();
+
+            trashFolderServices.Should().NotBeNull();
+
+            trashFolderServices = serviceProvider.GetRequiredService<ITrashFolderServices>();
+
+            trashFolderServices.Should().NotBeNull();
+
+            var idGenerators = serviceProvider.GetRequiredService<IIdGenerator<long>>();
+
+            idGenerators.Should().NotBeNull();
+        }
+    }
+}
